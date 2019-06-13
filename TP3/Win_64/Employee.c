5530a9956cb9561e734ae130fd8dc5233b066c5b
LucianoPezza <47321345+LucianoPezza@users.noreply.github.com>
2019-06-13 12:14:31 -0300

    Add files via upload

---
+++ TP3/Win_64/Employee.c
@@ -0,0 +1,17 @@
+#include "Employee.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+Employee* employee_new()
+{
+    Employee* empleadito;
+    empleadito=(Employee*)calloc(sizeof(Employee),1);
+    return empleadito;
+}
+
+Employee* employee_newParametros(char* idStr,char* nombreStr,char* horasTrabajadasStr)
+{
+
+}
+
---
+++ TP3/Win_64/parser.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LinkedList.h"
+#include "Employee.h"
+
+/** \brief Parsea los datos los datos de los empleados desde el archivo data.csv (modo texto).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int parser_EmployeeFromText(FILE* pFile , LinkedList* pArrayListEmployee)
+{
+    char auxId;
+    char auxNombre[128];
+    char auxHorasTrabajadas;
+    char auxSueldo;
+    int r;
+
+        if(pFile == NULL)
+        {
+            return -1;
+        }
+        do
+        {
+            r = fscanf(pFile,"%[^,],%[^,],%[^,],%[^,\n]\n,",auxId, auxNombre, auxSueldo, auxHorasTrabajadas);
+        }while(!feof(pFile));
+
+        return 1;
+    }
+
+
+
+
+/** \brief Parsea los datos los datos de los empleados desde el archivo data.csv (modo binario).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int parser_EmployeeFromBinary(FILE* pFile , LinkedList* pArrayListEmployee)
+{
+
+    return 1;
+}
